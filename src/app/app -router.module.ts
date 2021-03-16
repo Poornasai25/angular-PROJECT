93f6376acde27000f9e382a4ed8c5d05cfdbba7e
Poornasai25 <poornasai2505@gmail.com>
2021-03-16 12:49:45 +0530

    2 files modified, 5 added

---
+++ src/app/app -router.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { homeComponent } from "./header/header.component";
+import { aboutComponent } from "./about/about.component";
+import { contantComponent } from "./contant/contant.component";
+
+const routes: Routes = [
+  { path: "home", component: homeComponent },
+  { path: "about", component: aboutComponent },
+  { path: "contant", component: contantComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContantComponent } from './contant/contant.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent, HomeComponent, FooterComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, HomeComponent, FooterComponent, AboutComponent, ContantComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
